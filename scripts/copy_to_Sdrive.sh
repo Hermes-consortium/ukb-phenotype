0df62555ea488b28b0f6d8d686465eb81c887cbe
rmhiaah <rmhiaah@login13.myriad.ucl.ac.uk>
2020-06-09 17:34:53 +0100

    add scripts from myriad

---
+++ scripts/copy_to_Sdrive.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Run from local
+# Scripts to copy file from Myriad to SDrive
+# Requires S Drive to be mounted first
+# https://www.ucl.ac.uk/isd/services/file-storage-sharing/groupfolders-shared-s-drive
+
+DATA_DIR="/lustre/scratch/scratch/rmhiaah/Projects/ukb-phenotype/data"
+FILES=(${DATA_DIR}/ukb42306.html ${DATA_DIR}/2020-06-08_ukb42306_app15422.tsv ${DATA_DIR}/ukb
+
+LOCAL_DIR="/Volumes/groupfolders/FPHS_IHI_DataLab_UKBiobank/application-15422/ukb-phenotype"
+
+scp "${FILES[@]}" "$LOCAL_DIR/data/"
---
+++ scripts/download_data.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Scripts to unpack UKB data
+# The encrypted data was downloaded separately directly from the AMS portal
+# For instructions, see https://biobank.ndph.ox.ac.uk/~bbdatan/Accessing_UKB_data_v2.1.pdf
+
+# The key and MD5sum was emailed directly by the UKB AMS k15422r42306.key, the following 2 lines were the content
+# 15422
+# a490a9bff5744c45b263f703e5dc0b550281f9a7a61c6720c4ebbaa0bc133bbd
+
+# Make sure that ukb utility tools have been downloaded and added to the $PATH
+# https://biobank.ndph.ox.ac.uk/showcase/download.cgi?ams=6484c40676f07664e51b0298b54c7ad4&tn=1591631913&ui=49854&ai=15422&bi=0&ed=1
+
+DATA_DIR="/lustre/scratch/scratch/rmhiaah/Projects/ukb-phenotype/data"
+cd $DATA_DIR
+
+FILE_ID="ukb42306"
+
+# STEP 1: check MD5sum ---------
+MD5_from_email="4cec31fdc88c218b54029ee4b623075e"
+MD5_downloaded=$(ukbmd5 ${FILE_ID}.enc | tail -n 1 MD5_downloaded.txt | sed 's/.*MD5=//g')
+
+if [[ $MD5_from_email == $MD5_downloaded ]]; then
+  echo "MD5 hash for the downloaded file is correct"
+else
+  echo "incorrect MD5 hash. Please check the downloaded file."
+fi
+
+# STEP 2: unpack encrypted file -----------
+ukbunpack ${FILE_ID}.enc k15422r42306.key
+
+# STEP 3: Conversion ---------
+# downloads encoding data (required by ukbconv)
+wget  -nd  biobank.ctsu.ox.ac.uk/crystal/util/encoding.ukb
+
+# create data dictionary
+ukbconv ${FILE_ID}.enc_ukb docs
+
+# convert to tsv
+ukbconv ${FILE_ID}.enc_ukb txt
+
+# rename and create a timestamp
+DATE=$(grep 'REX: ' ${FILE_ID}.log | awk -v FS='REX: |T' '{print $2}')
+APP_ID=$(grep 'Application ' ${FILE_ID}.log | awk -v FS='[ ,]' '{print $2}')
+mv ${FILE_ID}.txt "${DATE}_${FILE_ID}_app${APP_ID}.tsv"
