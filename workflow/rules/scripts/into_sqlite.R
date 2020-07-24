2db9583f4f11f48b40ff9a242720255129fa49fb
Albert <albert_henry@outlook.com>
2020-07-24 11:54:02 +0100

    update R script to put data into db

--- workflow/rules/scripts/into_sqlite.R
+++ workflow/rules/scripts/into_sqlite.R
@@ -3,13 +3,21 @@
 pacman::p_load(vroom, tidyverse, purrr, DBI, RSQLite)
 
 list_file <- snakemake@input
+db <- snakemake@output[[1]]
+params <- snakemake@params
 
-# for testing
+# for testing=============
 list_file <-
   paste0("data/app15422/by_type/ukb42306_",
          c("str", "int", "float", "multiCat", "singleCat"),
          "_long.csv") %>%
   as.list
+
+db <- "data/app15422/ukb42306.sqlite"
+params <- list(overwrite_db = F,
+               overwrite_table = F,
+               append_table = F)
+# ========================
 
 types <- map_chr(list_file, basename) %>% str_split("_") %>% map_chr(`[`, 2)
 
@@ -17,8 +25,33 @@ r_types <- case_when(types %in% c("int", "singleCat", "multiCat", "eid") ~ "i",
                     types %in% c("str") ~ "c",
                     types == "float" ~ "d") %>% set_names(types)
 
-list_df <- map2(list_file, types, ~vroom(.x, col_types=c(.default = r_type, eid = "i")))
+list_df <- map2(list_file, r_types, ~vroom(.x, col_types=c(.default = "i", value = .y))) %>% 
+  set_names(types)
 
-into_sqlite <- function(type){
+# Store into SQLite database
+if (params$overwrite_db) system(paste("rm -f", db))
 
+db_conn <- dbConnect(SQLite(), db)
+
+into_sqlite <- function(type, conn = db_conn,
+                        overwrite_table = params$overwrite_table,
+                        append_table = params$append_table){
+  dbWriteTable(conn, type, list_df[[type]],
+               overwrite = overwrite_table,
+               append = append_table)
 }
+
+walk(types, into_sqlite)
+
+# TEST =============
+# df <- dbGetQuery(db_conn, 'SELECT * FROM int WHERE value IS NOT NULL')
+#
+# df_str <- tbl(db_conn, "str")
+#
+# df_str %>% distinct(eid) %>% tally
+#
+# main_int_db %>% filter(!is.na(value))
+#
+# main_int_db %>% show_query()
+
+dbDisconnect(db)
